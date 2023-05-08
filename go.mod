49633c72c85b347f0fbb197b4bdf47eba046b0d0
Samuel Klein <it@samuel-klein.net>
2023-05-08 18:14:13 +0200

    added argparser

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/KleinSamuel/gtamap
 go 1.20
 
 require (
-	github.com/sirupsen/logrus v1.9.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	github.com/akamensky/argparse v1.4.0
+	github.com/sirupsen/logrus v1.9.0
 )
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- src/testrunner.go
+++ src/testrunner.go
@@ -6,7 +6,9 @@ import (
 	"github.com/KleinSamuel/gtamap/src/core/datastructure"
 	"github.com/KleinSamuel/gtamap/src/dataloader"
 	"github.com/KleinSamuel/gtamap/src/dataloader/gtf"
+	"github.com/akamensky/argparse"
 	"github.com/sirupsen/logrus"
+	"os"
 	"time"
 )
 
@@ -67,6 +69,79 @@ func search() {
 	fmt.Println("result: ", result)
 }
 
+func testArgparse() {
+	
+	parser := argparse.NewParser("gtamap", "Gene-based Trancript-Aware readMAPping")
+
+	var cmdIndex *argparse.Command = parser.NewCommand("index", "Build the GTAMap index (.gtai).")
+
+	var gtfFile *os.File = cmdIndex.File("", "gtf", os.O_RDONLY, 0600, &argparse.Options{
+		Required: true,
+		Help:     "Gene transfer format (GTF) file.",
+	})
+	var fastaFile *os.File = cmdIndex.File("", "fasta", os.O_RDONLY, 0600, &argparse.Options{
+		Required: true,
+		Help:     "Nucleotide sequences (FASTA) file. The corresponding index (.fai) file must be present.",
+	})
+	var outputFile *os.File = cmdIndex.File("o", "output", os.O_WRONLY|os.O_CREATE, 0600, &argparse.Options{
+		Required: true,
+		Help:     "Output file (.gtai).",
+	})
+	var logLevelIndex *string = cmdIndex.Selector("", "loglevel", []string{"ERROR", "INFO", "DEBUG"}, &argparse.Options{
+		Required: false,
+		Help:     "Log output level.",
+		Default:  "ERROR",
+	})
+
+	var cmdMap *argparse.Command = parser.NewCommand("map", "Map reads to the GTAMap index.")
+	var indexFile *os.File = cmdMap.File("", "index", os.O_RDONLY, 0600, &argparse.Options{
+		Required: true,
+		Help:     "GTAMap index (.gtai) file",
+	})
+	var fastqFwFile *os.File = cmdMap.File("", "r1", os.O_RDONLY, 0600, &argparse.Options{
+		Required: true,
+		Help:     "FASTQ file containing the forward reads.",
+	})
+	var fastqRwFile *os.File = cmdMap.File("", "r2", os.O_RDONLY, 0600, &argparse.Options{
+		Required: false,
+		Help:     "FASTQ file containing the reverse reads.",
+	})
+	var logLevelMap *string = cmdMap.Selector("", "loglevel", []string{"ERROR", "INFO", "DEBUG"}, &argparse.Options{
+		Required: false,
+		Help:     "log output level",
+		Default:  "ERROR",
+	})
+
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		return
+	}
+
+	if cmdIndex.Happened() {
+
+		fmt.Println("Indexing..")
+
+		fmt.Println("GTF file: ", *gtfFile)
+		fmt.Println("FASTA file: ", *fastaFile)
+		fmt.Println("Output file: ", *outputFile)
+		fmt.Println("loglevel: ", *logLevelIndex)
+
+	} else if cmdMap.Happened() {
+
+		fmt.Println("Mapping..")
+
+		fmt.Println("loglevel: ", *logLevelMap)
+		fmt.Println("index: ", *indexFile)
+		fmt.Println("fastq fw: ", *fastqFwFile)
+		fmt.Println("fastq rw: ", *fastqRwFile)
+
+	} else {
+		fmt.Println(parser.Usage("no valid command supplied"))
+		return
+	}
+}
+
 func main() {
 
 	logrus.SetFormatter(&logrus.TextFormatter{
@@ -74,5 +149,5 @@ func main() {
 	})
 	logrus.SetLevel(logrus.InfoLevel)
 
-	buildAndSerializeSuffixTree()
+	testArgparse()
 }
