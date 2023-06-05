c4d411c3e6f0b85817325af2fbd8977e22663ca3
Samuel Klein <klein@bio.ifi.lmu.de>
2023-06-05 18:58:44 +0200

    renamed fw and rv reads to R1 and R2 for fastq reader

--- src/dataloader/fastq/fastq.go
+++ src/dataloader/fastq/fastq.go
@@ -14,70 +14,68 @@ type Read struct {
 }
 
 type ReadPair struct {
-	FwRead *Read
-	RvRead *Read
+	ReadR1 *Read
+	ReadR2 *Read
 }
 
 type Reader struct {
-	scannerFw *bufio.Scanner
-	scannerRv *bufio.Scanner
+	scannerR1 *bufio.Scanner
+	scannerR2 *bufio.Scanner
 }
 
-func InitFromPaths(pathFwReads string, pathRvReads string) *Reader {
+func InitFromPaths(pathR1Reads string, pathR2Reads string) *Reader {
 
 	logrus.WithFields(logrus.Fields{
-		"forward": pathFwReads,
-		"reverse": pathRvReads,
+		"R1": pathR1Reads,
+		"R2": pathR2Reads,
 	}).Debug("Initializing reader")
 
-	var fileFwReads *os.File = nil
-	var errFw error = nil
-	var scannerFw *bufio.Scanner = nil
+	var fileR1Reads *os.File = nil
+	var errR1 error = nil
+	var scannerR1 *bufio.Scanner = nil
 
-	var fileRvReads *os.File = nil
-	var errRv error = nil
-	var scannerRv *bufio.Scanner = nil
+	var fileR2Reads *os.File = nil
+	var errR2 error = nil
+	var scannerR2 *bufio.Scanner = nil
 
-	fileFwReads, errFw = os.Open(pathFwReads)
-	if errFw != nil {
-		logrus.Fatal("Error reading forward reads", errFw)
+	fileR1Reads, errR1 = os.Open(pathR1Reads)
+	if errR1 != nil {
+		logrus.Fatal("Error reading R1 reads", errR1)
 	}
-	scannerFw = bufio.NewScanner(fileFwReads)
+	scannerR1 = bufio.NewScanner(fileR1Reads)
 
-	logrus.Debug("Initialized reader with forward reads")
+	logrus.Debug("Initialized reader with R1 reads")
 
-	// If reverse reads are given, open them as well
-	if len(pathRvReads) > 0 {
+	// if R2 reads are given, open them as well
+	if len(pathR2Reads) > 0 {
 		fmt.Println("Reverse reads given")
-		fileRvReads, errRv = os.Open(pathRvReads)
-		if errRv != nil {
-			logrus.Fatal("Error reading reverse reads", errRv)
+		fileR2Reads, errR2 = os.Open(pathR2Reads)
+		if errR2 != nil {
+			logrus.Fatal("Error reading R2 reads", errR2)
 		}
-		scannerRv = bufio.NewScanner(fileRvReads)
+		scannerR2 = bufio.NewScanner(fileR2Reads)
 
-		logrus.Debug("Initialized reader with reverse reads")
+		logrus.Debug("Initialized reader with R2 reads")
 	}
 
 	return &Reader{
-		scannerFw: scannerFw,
-		scannerRv: scannerRv,
+		scannerR1: scannerR1,
+		scannerR2: scannerR2,
 	}
 }
 
 func (r Reader) NextRead() *ReadPair {
 
-	if !r.scannerFw.Scan() {
+	if !r.scannerR1.Scan() {
 		return nil
 	}
 
-	//fmt.Println("Next read")
-
-	header := r.scannerFw.Text()
-	r.scannerFw.Scan()
-	sequence := r.scannerFw.Text()
-	r.scannerFw.Scan()
-	r.scannerFw.Scan()
-	quality := r.scannerFw.Text()
+	header := r.scannerR1.Text()
+	r.scannerR1.Scan()
+	sequence := r.scannerR1.Text()
+	r.scannerR1.Scan()
+	r.scannerR1.Scan()
+	quality := r.scannerR1.Text()
 
 	var fwRead *Read = &Read{
 		Header:   header,
@@ -86,17 +84,17 @@ func (r Reader) NextRead() *ReadPair {
 	}
 	var rvRead *Read = nil
 
-	if r.scannerRv != nil {
-		if !r.scannerRv.Scan() {
+	if r.scannerR2 != nil {
+		if !r.scannerR2.Scan() {
 			return nil
 		}
 
-		header := r.scannerRv.Text()
-		r.scannerRv.Scan()
-		sequence := r.scannerRv.Text()
-		r.scannerRv.Scan()
-		r.scannerRv.Scan()
-		quality := r.scannerRv.Text()
+		header := r.scannerR2.Text()
+		r.scannerR2.Scan()
+		sequence := r.scannerR2.Text()
+		r.scannerR2.Scan()
+		r.scannerR2.Scan()
+		quality := r.scannerR2.Text()
 
 		rvRead = &Read{
 			Header:   header,
@@ -106,7 +104,7 @@ func (r Reader) NextRead() *ReadPair {
 	}
 
 	return &ReadPair{
-		FwRead: fwRead,
-		RvRead: rvRead,
+		ReadR1: fwRead,
+		ReadR2: rvRead,
 	}
 }
