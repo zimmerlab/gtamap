aa31ea06c69864f7def8df7c304dc6580b8daf8c
Malte Weyrich <120316140+mweyrich28@users.noreply.github.com>
2025-05-13 15:03:27 +0200

    adding sparseSam parser for testing

--- src/formats/sam/sam.go
+++ src/formats/sam/sam.go
@@ -1,6 +1,15 @@
 package sam
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"github.com/KleinSamuel/gtamap/src/core/datastructure/regionvector"
+	"github.com/KleinSamuel/gtamap/src/core/mapper/mapperutils"
+	"io"
+	"os"
+	"strconv"
+)
 
 const Version string = "1.6"
 
@@ -256,4 +265,246 @@ func (b *FlagBuilder) SetSupplementary() *FlagBuilder {
 
 func (b *FlagBuilder) Build() Flag {
 	return b.flag
+}
+
+// NOTE: I only added this for testing
+type CigarOp struct {
+	Length int
+	Op     byte
+}
+
+func ParseCIGAR(cigar []byte) ([]CigarOp, error) {
+
+	var ops []CigarOp
+
+	start := 0
+	for i := 0; i < len(cigar); i++ {
+		// as soon as we hit a non-numeric byte, we can create a new cigarOp
+		if cigar[i] < '0' || cigar[i] > '9' {
+			cigraPrefix, err := strconv.Atoi(string(cigar[start:i]))
+			if err != nil {
+				return nil, err
+			}
+			ops = append(ops, CigarOp{Op: cigar[i], Length: cigraPrefix})
+			start = i + 1
+		}
+	}
+
+	return ops, nil
+}
+
+type SAMIndexEntry struct {
+	Name           []byte
+	Offset         int64
+	FirstOfPair    bool
+	DelimPositions [11]uint16 // this is hardcoded since we know how many fields each sam entry has. Using this array, we can quickly jump to a desired field
+}
+
+type SAMReadPair struct {
+	First  *SAMIndexEntry
+	Second *SAMIndexEntry
+}
+
+func ParseDelims(line []byte) ([]byte, [11]uint16, bool) {
+	tabCount := 0
+	delimPositions := [11]uint16{}
+	var name []byte
+	var flagStart int
+	var isFist bool
+	for i := 0; i < len(line); i++ {
+		if line[i] == '\t' {
+
+			if tabCount == 0 {
+				name = make([]byte, i)
+				copy(name, line[0:i])
+				flagStart = i + 1
+			}
+			if tabCount == 1 {
+				flagBytes := line[flagStart:i]
+				flagInt, err := strconv.Atoi(string(flagBytes))
+				if err != nil {
+					panic("Error parsing flag of read")
+				}
+				isFist = flagInt&0x40 != 0
+			}
+			delimPositions[tabCount] = uint16(i)
+			tabCount++
+		}
+
+	}
+	return name, delimPositions, isFist
+}
+
+// ParseSparseSAMEntry takes a file and a sparseSAMEntry and parses the corresponding sam entry
+func ParseSparseSAMEntry(file *os.File, sparseSAMEntry *SAMIndexEntry) (*mapperutils.ReadMatchResult, []byte, []byte, string, error) {
+	if sparseSAMEntry == nil || (sparseSAMEntry.Offset < 0) {
+		return nil, nil, nil, "", errors.New("invalid read: no offset available")
+	}
+	offset := sparseSAMEntry.Offset
+
+	// jump to read pos in sam
+	_, err := file.Seek(offset, io.SeekStart)
+	if err != nil {
+		return nil, nil, nil, "", err
+	}
+
+	reader := bufio.NewReader(file)
+	line, err := reader.ReadBytes('\n')
+	if err != nil && err != io.EOF {
+		return nil, nil, nil, "", err
+	}
+
+	// remove trailing newline if present
+	if len(line) > 0 && line[len(line)-1] == '\n' {
+		line = line[:len(line)-1]
+	}
+	startBytes := line[sparseSAMEntry.DelimPositions[2]+1 : sparseSAMEntry.DelimPositions[3]]
+	qnameBytes := line[0:sparseSAMEntry.DelimPositions[0]]
+	qualityBytes := line[sparseSAMEntry.DelimPositions[3]+1 : sparseSAMEntry.DelimPositions[4]]
+	cigarBytes := line[sparseSAMEntry.DelimPositions[4]+1 : sparseSAMEntry.DelimPositions[5]]
+	sequenceBytes := line[sparseSAMEntry.DelimPositions[8]+1 : sparseSAMEntry.DelimPositions[9]]
+
+	startPos, err := strconv.Atoi(string(startBytes))
+	if err != nil {
+		return nil, nil, nil, "", err
+	}
+
+	var sId int
+	if sparseSAMEntry.FirstOfPair {
+		sId = 0
+	} else {
+		sId = 1
+	}
+
+	result := &mapperutils.ReadMatchResult{
+		SequenceIndex: sId,
+		FourthPass:    false, // this is always false since the read is listed in the output sam
+	}
+
+	result.MatchedRead = regionvector.NewRegionVector()
+	result.MatchedGenome = regionvector.NewRegionVector()
+
+	// Process CIGAR string to build region vectors and identify mismatches
+	err = processCigarForRegions(cigarBytes, startPos, sequenceBytes, result)
+	if err != nil {
+		return nil, nil, nil, "", err
+	}
+
+	return result, sequenceBytes, qualityBytes, string(qnameBytes), nil
+}
+
+// processCigarForRegions analyzes the CIGAR string to build read and genome region vectors
+func processCigarForRegions(cigar []byte, refPos int, readSeq []byte, result *mapperutils.ReadMatchResult) error {
+	readPos := 0
+
+	// get cigar ops
+	cigarOps, err := ParseCIGAR(cigar)
+	if err != nil {
+		return err
+	}
+
+	for _, op := range cigarOps {
+		switch op.Op {
+		case 'M', '=', 'X':
+			result.MatchedRead.AddRegionNonOverlappingPanic(readPos, readPos+op.Length)
+			result.MatchedGenome.AddRegionNonOverlappingPanic(refPos, refPos+op.Length)
+
+			if op.Op == 'X' {
+				for i := 0; i < op.Length; i++ {
+					result.MismatchesRead = append(result.MismatchesRead, readPos+i)
+				}
+			}
+
+			readPos += op.Length
+			refPos += op.Length
+
+		case 'I':
+			readPos += op.Length
+
+		case 'D', 'N':
+			refPos += op.Length
+
+		case 'S':
+			readPos += op.Length
+
+		case 'H':
+			// nothing is consumed
+
+		case 'P':
+			// nothing is consumed
+		}
+	}
+
+	return nil
+}
+
+func CreateSAMIndex(samfile *os.File) map[string]*SAMReadPair {
+
+	scanner := bufio.NewScanner(samfile)
+	const maxCapacity = 1024 * 1024 // 1MB
+	buf := make([]byte, maxCapacity)
+	scanner.Buffer(buf, maxCapacity)
+
+	samIndex := make(map[string]*SAMReadPair)
+
+	offset := int64(0)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		if line[0] == '@' {
+			offset += int64(len(line) + 1)
+			continue
+		}
+
+		name, delimPositions, firstOfPair := parseDelims(line)
+		indexEntry := &SAMIndexEntry{
+			Name:           name,
+			Offset:         offset,
+			FirstOfPair:    firstOfPair,
+			DelimPositions: delimPositions,
+		}
+		strName := string(name)
+		_, exists := samIndex[strName]
+		if !exists {
+			samIndex[strName] = &SAMReadPair{}
+		}
+
+		if firstOfPair {
+			samIndex[strName].First = indexEntry
+		} else {
+			samIndex[strName].Second = indexEntry
+		}
+
+		offset += int64(len(line)) + 1 // +1 for newline
+	}
+
+	return samIndex
+}
+func parseDelims(line []byte) ([]byte, [11]uint16, bool) {
+	tabCount := 0
+	delimPositions := [11]uint16{}
+	var name []byte
+	var flagStart int
+	var isFist bool
+	for i := 0; i < len(line); i++ {
+		if line[i] == '\t' {
+
+			if tabCount == 0 {
+				name = make([]byte, i)
+				copy(name, line[0:i])
+				flagStart = i + 1
+			}
+			if tabCount == 1 {
+				flagBytes := line[flagStart:i]
+				flagInt, err := strconv.Atoi(string(flagBytes))
+				if err != nil {
+					panic("Error parsing flag of read")
+				}
+				isFist = flagInt&0x40 != 0
+			}
+			delimPositions[tabCount] = uint16(i)
+			tabCount++
+		}
+
+	}
+	return name, delimPositions, isFist
 }
