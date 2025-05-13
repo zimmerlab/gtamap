35a2e39f959a924706e7bc85de25dd9a5c593d81
Malte Weyrich <120316140+mweyrich28@users.noreply.github.com>
2025-05-13 15:02:54 +0200

    experimenting with postmappingworker

--- src/core/mapper/postmappingworker.go
+++ src/core/mapper/postmappingworker.go
@@ -3,6 +3,7 @@ package mapper
 import (
 	"fmt"
 	"github.com/KleinSamuel/gtamap/src/core/algorithms"
+	"github.com/KleinSamuel/gtamap/src/core/datastructure/graph"
 	"github.com/KleinSamuel/gtamap/src/core/mapper/mappedreadpair"
 	"github.com/sirupsen/logrus"
 	"sync"
@@ -10,15 +11,15 @@ import (
 
 func PostMappingWorker(mappedReadPairChan <-chan *mappedreadpair.ReadPairMatchResult, wg *sync.WaitGroup, outputChan chan<- string) {
 
-	logrus.Debug("Started accumulating mapped readpairs")
+	logrus.Info("Started accumulating mapped readpairs")
 
 	// in intervalsPerSeq, the key 0 references GenomeIndex.Sequences[0]
 	// that's why we do intervalsPerSeq[mappedReadPair.Fw.SequenceIndex/2] and intervalsPerSeq[mappedReadPair.Rv.SequenceIndex/2]
 	// it projects 0,1 -> 0; 2,3 -> 1; 4,5 -> 2
-	results := make(map[int][]*mappedreadpair.ReadPairMatchResult)
+	resultsPerSeqIndex := make(map[int][]*mappedreadpair.ReadPairMatchResult)
 	intervalsPerSeq := make(map[int][][2]int)
 	for mappedReadPair := range mappedReadPairChan {
-		results[mappedReadPair.Fw.SequenceIndex/2] = append(results[mappedReadPair.Fw.SequenceIndex/2], mappedReadPair)
+		resultsPerSeqIndex[mappedReadPair.Fw.SequenceIndex/2] = append(resultsPerSeqIndex[mappedReadPair.Fw.SequenceIndex/2], mappedReadPair)
 		for _, region := range mappedReadPair.Fw.MatchedGenome.Regions {
 			intervalsPerSeq[mappedReadPair.Fw.SequenceIndex/2] = append(intervalsPerSeq[mappedReadPair.Fw.SequenceIndex/2], [2]int{region.Start, region.End})
 		}
@@ -30,6 +31,7 @@ func PostMappingWorker(mappedReadPairChan <-chan *mappedreadpair.ReadPairMatchRe
 			intervalsPerSeq[mappedReadPair.Rv.SequenceIndex/2] = append(intervalsPerSeq[mappedReadPair.Rv.SequenceIndex/2], [2]int{convStart, convEnd})
 		}
 	}
+	logrus.Info("Finished accumulating mapped readpairs")
 
 	for seqId, intervals := range intervalsPerSeq {
 		fmt.Println(seqId)
@@ -38,12 +40,26 @@ func PostMappingWorker(mappedReadPairChan <-chan *mappedreadpair.ReadPairMatchRe
 	}
 
 	logrus.Debug("Identifying critical intervalsPerSeq")
-	fmt.Println(len(results))
+	fmt.Println(len(resultsPerSeqIndex))
 
-	for _, mappedReadPairs := range results {
+	for _, mappedReadPairs := range resultsPerSeqIndex {
+		og := graph.OverlapGraph{
+			Nodes:     map[int]*graph.Node{},
+			Index:     mappedReadPairs[0].Index,
+			NodeCount: 0,
+		}
+		for _, mappedReadPair := range mappedReadPairs {
+			og.InsertNode(mappedReadPair)
+		}
+		fmt.Println(og.ToString())
+		fmt.Println()
+	}
+
+	// WRITE TO SAM
+	for _, mappedReadPairs := range resultsPerSeqIndex {
 		for _, mappedReadPair := range mappedReadPairs {
 			fwRes := mappedReadPair.Fw
-			rvRes := mappedReadPair.Fw
+			rvRes := mappedReadPair.Rv
 			output, isMapping := FormatMappedReadPairToSAM(*fwRes, *rvRes, mappedReadPair.ReadPair, mappedReadPair.Index)
 			if isMapping {
 				outputChan <- output
