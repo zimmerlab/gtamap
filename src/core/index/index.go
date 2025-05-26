d13fe90b7374770c756b689682537eb9efa57e95
Malte Weyrich <120316140+mweyrich28@users.noreply.github.com>
2025-05-26 16:46:44 +0200

    fixing log placement

--- src/core/index/index.go
+++ src/core/index/index.go
@@ -955,9 +955,9 @@ func OptimizeFastaExtraction(targetParalogs map[string]map[string]struct{}, fast
 				_, exists := paralogs[faFileName]
 				if exists {
 					target = t
+					logrus.Infof("Found paralog region '%s' of target region '%s' in --fastaout '%s'. No sequence extraction necessary.", faFileName, target, *fastaDirParalogPre)
 				}
 			}
-			logrus.Infof("Found paralog region '%s' of target region '%s' in --fastaout '%s'. No sequence extraction necessary.", faFileName, target, *fastaDirParalogPre)
 			existingFaFiles[faFileName] = struct{}{}
 			foundFiles++
 		}
