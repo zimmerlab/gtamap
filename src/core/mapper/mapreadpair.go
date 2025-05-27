b693890d10c6b34d9b52fa4ff7494c09cde79bd7
Malte Weyrich <120316140+mweyrich28@users.noreply.github.com>
2025-05-27 17:24:06 +0200

    fixed bug in if statement
    forgot braces - that way A && B && C || D was also true if only D was true
    
    now i added (A && B && (C || D)

--- src/core/mapper/mapreadpair.go
+++ src/core/mapper/mapreadpair.go
@@ -65,7 +65,7 @@ func MapReadPair(readPair *fastq.ReadPair, genomeIndex *index.GenomeIndex,
 	})
 
 	// check if confident map
-	if len(resultFw) == 1 && len(resultRv) == 1 && len(resultFw[0].MismatchesRead)+len(resultRv[0].MismatchesRead) < 6 && resultRv[0].SequenceIndex-1 == resultFw[0].SequenceIndex || resultRv[0].SequenceIndex == resultFw[0].SequenceIndex-1 { // currently just sending everything
+	if len(resultFw) == 1 && len(resultRv) == 1 && len(resultFw[0].MismatchesRead)+len(resultRv[0].MismatchesRead) < 6 && (resultRv[0].SequenceIndex-1 == resultFw[0].SequenceIndex || resultRv[0].SequenceIndex == resultFw[0].SequenceIndex-1) { // currently just sending everything
 		confidentMatchesChan.Send(&confidentmappingpass.ConfidentTask{
 			ReadPair: readPair,
 			ResultFw: resultFw[0].Copy(), // there should only exist fw[0] and rv[0] in a confident match
