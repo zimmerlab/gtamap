e7ca2ab622c3db7b7fce3d1c105b6622ed8dd1ec
Samuel Klein <it@samuel-klein.net>
2025-06-25 17:34:28 +0200

    added routest to server to provide igv information

--- src/server/server.go
+++ src/server/server.go
@@ -7,12 +7,13 @@ import (
 	"github.com/rs/cors"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func enableCORS() *cors.Cors {
 	return cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:8080", "http://localhost:3000"},
+		AllowedOrigins: []string{"http://localhost:8080", "http://localhost:5173"},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders: []string{"*"},
 	})
@@ -27,22 +28,135 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func serveGenomeFastaFile(w http.ResponseWriter, r *http.Request) {
+
+	filePath := "/home/sam/Projects/gtamap-paper/pipeline/input/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+
+	http.ServeFile(w, r, filePath)
+}
+
+func serveGenomeFastaIndexFile(w http.ResponseWriter, r *http.Request) {
+
+	filePath := "/home/sam/Projects/gtamap-paper/pipeline/input/Homo_sapiens.GRCh38.dna.primary_assembly.fa.fai"
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+
+	http.ServeFile(w, r, filePath)
+}
+
+func serveGenomeAnnotationFile(w http.ResponseWriter, r *http.Request) {
+
+	filePath := "/home/sam/Projects/gtamap-paper/pipeline/input/Homo_sapiens.GRCh38.113.gtf"
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+
+	http.ServeFile(w, r, filePath)
+}
+
+func serveBamFile(w http.ResponseWriter, r *http.Request) {
+
+	filePath := "/home/sam/Projects/gtamap-paper/pipeline/output/ENSG00000173585/ENSG00000173585.gtamap.target.bam"
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+
+	http.ServeFile(w, r, filePath)
+}
+
+func serveBamIndexFile(w http.ResponseWriter, r *http.Request) {
+
+	filePath := "/home/sam/Projects/gtamap-paper/pipeline/output/ENSG00000173585/ENSG00000173585.gtamap.target.bam.bai"
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+
+	http.ServeFile(w, r, filePath)
+}
+
+type IgvGenomeConfig struct {
+	Id       string `json:"id"`
+	Label    string `json:"label"`
+	FastaUrl string `json:"fastaURL"`
+	IndexUrl string `json:"indexURL"`
+}
+
+type IgvTrackConfig struct {
+	Name        string `json:"name"`
+	Format      string `json:"format"`
+	DisplayMode string `json:"displayMode"`
+	Url         string `json:"url"`
+	IndexUrl    string `json:"indexURL"`
+	Type        string `json:"type"`
+}
+
+func getTestGenomeConfig(w http.ResponseWriter, r *http.Request) {
+
+	genomeConfig := IgvGenomeConfig{
+		Id:       "BASE_GENOME",
+		Label:    "BASE_GENOME_LABEL",
+		FastaUrl: "http://localhost:8000/download/genome/fasta",
+		IndexUrl: "http://localhost:8000/download/genome/fastaIndex",
+	}
+
+	track := IgvTrackConfig{
+		Name:        "TEST_TRACK",
+		Format:      "bam",
+		DisplayMode: "EXPANDED",
+		Url:         "http://localhost:8000/download/gtamap/bam",
+		IndexUrl:    "http://localhost:8000/download/gtamap/bamIndex",
+		Type:        "alignment",
+	}
+
+	tracks := make([]IgvTrackConfig, 0)
+	tracks = append(tracks, track)
+
+	response := map[string]interface{}{
+		"genomeConfig": genomeConfig,
+		"tracks":       tracks,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
 func StartServer() {
 
-	// Create router
 	r := mux.NewRouter()
 
-	// API routes
 	api := r.PathPrefix("/api").Subrouter()
+
 	api.HandleFunc("/health", healthCheck).Methods("GET")
 
-	// Setup CORS
+	api.HandleFunc("/genomeConfig", getTestGenomeConfig).Methods("GET")
+
+	download := r.PathPrefix("/download").Subrouter()
+
+	download.HandleFunc("/genome/fasta", serveGenomeFastaFile).Methods("GET")
+	download.HandleFunc("/genome/fastaIndex", serveGenomeFastaIndexFile).Methods("GET")
+	download.HandleFunc("/genome/gtf", serveGenomeAnnotationFile).Methods("GET")
+	download.HandleFunc("/gtamap/bam", serveBamFile).Methods("GET")
+	download.HandleFunc("/gtamap/bamIndex", serveBamIndexFile).Methods("GET")
+
 	c := enableCORS()
 	handler := c.Handler(r)
 
-	// Start server
 	port := ":8000"
+
 	fmt.Printf("Server starting on port %s\n", port)
 	log.Fatal(http.ListenAndServe(port, handler))
-
 }
