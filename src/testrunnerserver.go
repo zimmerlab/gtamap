f579d55e487d5c2ec7a6468e668bf27ea0920ddf
Samuel Klein <it@samuel-klein.net>
2025-06-27 17:17:48 +0200

    added more mapping results

--- src/testrunnerserver.go
+++ src/testrunnerserver.go
@@ -10,35 +10,44 @@ func main() {
 	s := server.NewServer()
 
 	gtamapTargetPath := "/home/sam/Projects/gtamap-paper/pipeline/output/ENSG00000173585/ENSG00000173585.gtamap.target.sam"
-
 	if err := s.AnalysisService.AddMapperInfo("gtamap", gtamapTargetPath); err != nil {
 		log.Fatalf("Error initializing analysis service: %v", err)
 	}
 
 	minimap2GenomePath := "/home/sam/Projects/gtamap-paper/pipeline/output/ENSG00000173585/ENSG00000173585.minimap2.genome.sam"
-
 	if err := s.AnalysisService.AddMapperInfo("minimap2", minimap2GenomePath); err != nil {
 		log.Fatalf("Error initializing analysis service: %v", err)
 	}
 
 	starGenomePath := "/home/sam/Projects/gtamap-paper/pipeline/output/ENSG00000173585/ENSG00000173585.star.genome.sam"
-
 	if err := s.AnalysisService.AddMapperInfo("star", starGenomePath); err != nil {
 		log.Fatalf("Error initializing analysis service: %v", err)
 	}
 
 	hisat2GenomePath := "/home/sam/Projects/gtamap-paper/pipeline/output/ENSG00000173585/ENSG00000173585.hisat2.genome.sam"
-
 	if err := s.AnalysisService.AddMapperInfo("hisat2", hisat2GenomePath); err != nil {
 		log.Fatalf("Error initializing analysis service: %v", err)
 	}
 
 	bbmapGenomePath := "/home/sam/Projects/gtamap-paper/pipeline/output/ENSG00000173585/ENSG00000173585.bbmap.genome.sam"
-
 	if err := s.AnalysisService.AddMapperInfo("bbmap", bbmapGenomePath); err != nil {
 		log.Fatalf("Error initializing analysis service: %v", err)
 	}
 
-	s.Start()
+	bowtie2GenomePath := "/home/sam/Projects/gtamap-paper/pipeline/output/ENSG00000173585/ENSG00000173585.bowtie2.genome.sam"
+	if err := s.AnalysisService.AddMapperInfo("bowtie2", bowtie2GenomePath); err != nil {
+		log.Fatalf("Error initializing analysis service: %v", err)
+	}
 
+	bwaMemGenomePath := "/home/sam/Projects/gtamap-paper/pipeline/output/ENSG00000173585/ENSG00000173585.bwa-mem.genome.sam"
+	if err := s.AnalysisService.AddMapperInfo("bwa-mem", bwaMemGenomePath); err != nil {
+		log.Fatalf("Error initializing analysis service: %v", err)
+	}
+
+	bwaMem2GenomePath := "/home/sam/Projects/gtamap-paper/pipeline/output/ENSG00000173585/ENSG00000173585.bwa-mem2.genome.sam"
+	if err := s.AnalysisService.AddMapperInfo("bwa-mem2", bwaMem2GenomePath); err != nil {
+		log.Fatalf("Error initializing analysis service: %v", err)
+	}
+
+	s.Start()
 }
