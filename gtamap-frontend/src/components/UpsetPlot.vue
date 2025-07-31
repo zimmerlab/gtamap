342d11cbe968995b2860cf26f1b74498218ae43b
Samuel Klein <it@samuel-klein.net>
2025-07-31 17:25:24 +0200

    extracted upset plot

--- gtamap-frontend/src/components/UpsetPlot.vue
+++ gtamap-frontend/src/components/UpsetPlot.vue
@@ -1,10 +1,124 @@
-<script setup lang="ts">
+<template>
+  <div ref="plotDiv"></div>
+</template>
+
+<script setup>
+
+import { ref, onBeforeUnmount, defineProps, watch, defineEmits } from 'vue';
+
+import * as UpSetJS from '@upsetjs/bundle'
+
+const props = defineProps({
+  upsetData: {
+    type: Array,
+    default: () => []
+  },
+  title: {
+    type: String,
+    default: "Upset Plot"
+  }
+})
+
+const emit = defineEmits(['onSetClick'])
+
+const plotDiv = ref(null);
+
+const upsetDataRecordPos = ref({
+  sets: [],
+  combinations: []
+});
+
+const processUpsetData = function(elems) {
+
+  const sets = UpSetJS.extractSets(elems, elem => elem.sets);
+  const combinations = UpSetJS.generateCombinations(sets, {
+    type: 'distinctIntersection'
+  });
+
+  // sort the combinations by cardinality (set size) and name
+  const nameToCombination = {};
+  for (const combination of combinations) {
+    nameToCombination[combination.name] = combination;
+  }
+  const sortSets = (a, b) => {
+    const sizeA = nameToCombination[a.name] ? nameToCombination[a.name].cardinality : 0;
+    const sizeB = nameToCombination[b.name] ? nameToCombination[b.name].cardinality : 0;
+    const sizeDiff = sizeB - sizeA;
+    return sizeDiff !== 0 ? sizeDiff : a.name.localeCompare(b.name);
+  };
+  const sortCombinations = (a, b) => {
+    const sizeDiff = b.cardinality - a.cardinality;
+    return sizeDiff !== 0 ? sizeDiff : a.name.localeCompare(b.name);
+  };
+  sets.sort(sortSets);
+  combinations.sort(sortCombinations);
+  // end sort by cardinality and name
+
+  upsetDataRecordPos.value.sets = sets;
+  upsetDataRecordPos.value.combinations = combinations;
+
+  renderUpsetRecordPos();
+}
+
+let selectionUpsetRecordPos = ref(null);
+
+function onHoverUpsetRecordPos(set) {
+  selectionUpsetRecordPos.value = set;
+  renderUpsetRecordPos();
+}
+
+function onClickUpsetRecordPos(set) {
+  emit('onSetClick', set);
+}
+
+function renderUpsetRecordPos() {
+
+  if (!plotDiv.value) {
+    console.error("Plot div is not available");
+    return;
+  }
+
+  let sets = upsetDataRecordPos.value.sets
+  let combinations = upsetDataRecordPos.value.combinations
+  const upsetConfig = {
+    sets,
+    combinations,
+    width: 800,
+    height: 300,
+    selection: selectionUpsetRecordPos.value,
+    onHover: onHoverUpsetRecordPos,
+    onClick: onClickUpsetRecordPos,
+    color: "darkorchid",
+    selectionColor: "#65cc32",
+    //hoverHintColor: "#cc9932",
+    //hasSelectionColor: "#cc9932",
+    //alternatingBackgroundColor: true,
+    //hasSelectionOpacity: 0.5,
+    title: props.title,
+    fontSizes: {
+      barLabel: "8pt",
+      chartLabel: "8pt",
+      axisTick: "8pt",
+      setLabel: "8pt",
+      title: "11pt",
+    }
+  }
+  UpSetJS.render(plotDiv.value, upsetConfig);
+}
+
+watch(() => props.upsetData, (newData) => {
+  if (newData && newData.length > 0) {
+    processUpsetData(newData);
+  }
+}, { immediate: true });
+
+onBeforeUnmount(() => {
+  if (plotDiv.value) {
+    plotDiv.value.innerHTML = ''
+  }
+})
 
 </script>
-
-<template>
-  $END$
-</template>
 
 <style scoped>
 
--- gtamap-frontend/src/pages/TestPage.vue
+++ gtamap-frontend/src/pages/TestPage.vue
@@ -1,10 +1,20 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  $END$
+  <UpsetPlotReadPos title="test title" url="/api/upsetDataRecordPosCigar"></UpsetPlotReadPos>
 </template>
+
+<script>
+import UpsetPlot from "../components/UpsetPlot.vue";
+import UpsetPlotReadPos from "../components/UpsetPlotReadPos.vue";
+
+export default {
+  name: 'TestPage',
+  components: {UpsetPlotReadPos, UpsetPlot},
+  setup() {
+    // This is where you can set up your component logic
+    // For example, you can import and use the UpsetPlot component here
+  }
+}
+</script>
 
 <style scoped>
 
