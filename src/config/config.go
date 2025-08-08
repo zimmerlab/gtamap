15705d037c8b3f4a0812d6f374f78c1e958ab61e
Malte Weyrich <120316140+mweyrich28@users.noreply.github.com>
2025-08-08 15:38:29 +0200

    tidying config

--- src/config/config.go
+++ src/config/config.go
@@ -47,17 +47,17 @@ var (
 	fragmentLengthDeviation int = 0
 )
 
+// Mapping params
 var (
-	MaxBranchPoints     int = 10 // how often if applyPossibleDiagonals allowed to branch
-	MaxConfMm           int = 6  // how many mm is a conf map allowed to have
-	MinConfAnchorLength int = 20 // how long does each ali block in a conf map have to be to be considered conf
+	MaxBranchPoints int = 10 // how often if applyPossibleDiagonals allowed to branch
 )
 
-// by default, an intron cluster only absorbes an incoming gap (extending its reach) if the delta
-// of gap.start/stop and cluster.start/stop is less than 100. This allows overlapping introns but
-// also resolves intron coord confilct within close proximity
+// ConfidentWorkerParams
 var (
-	IntronClusterDelta        int = 100
+	MaxConfMm                 int = 6   // how many mm is a conf map allowed to have
+	MinConfAnchorLengthRNA    int = 20  // how long does each ali block in a conf map have to be to be considered conf in RNA
+	MinConfAnchorLengthDNA    int = 50  // how long does each ali block in a conf map have to be to be considered conf in DNA
+	IntronClusterDelta        int = 100 // by default, an intron cluster only absorbes an incoming gap (extending its reach) if the delta of gap.start/stop and cluster.start/stop is less than 100. This allows overlapping introns but also resolves intron coord confilct within close proximity
 	IntronClusterRepairWindow int = 5
 )
 
@@ -66,6 +66,9 @@ var (
 	IncludeMMinSAM     bool = true  // if set to true, CIGAR will include "=" and "X" runes instead of only "M"
 	IncludeAllPairings bool = false // if set to true, do all vs all in SAM out (we can later implement a method which does that by also looling at tlen, etc)
 )
+
+// RNA/DNA Flag
+var IsOriginRNA bool = false
 
 func Env() string {
 	return env
