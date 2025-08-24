65c70ad0e55309176f013c67f27e7bbf49734a2b
Samuel Klein <it@samuel-klein.net>
2025-08-24 19:39:12 +0200

    added alias for imports

--- gtamap-frontend/vite.config.js
+++ gtamap-frontend/vite.config.js
@@ -1,11 +1,14 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        tailwindcss()
-    ],
+	plugins: [vue(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 })
