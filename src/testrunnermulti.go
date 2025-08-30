73e531b7a9df5ca4088c934577fbaa7d62deb706
Samuel Klein <it@samuel-klein.net>
2025-08-30 13:19:55 +0200

    working on parallel mapping to multiple target

---
+++ src/testrunnermulti.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/KleinSamuel/gtamap/src/core/index"
+	"github.com/KleinSamuel/gtamap/src/core/mapper"
+	"github.com/KleinSamuel/gtamap/src/dataloader"
+	"github.com/KleinSamuel/gtamap/src/datawriter"
+	"github.com/KleinSamuel/gtamap/src/formats/fastq"
+	"github.com/sirupsen/logrus"
+)
+
+func buildMultiFastaIndex(pathFasta string, pathOutput string, pathRepeatmasker string) {
+
+	fastaFile, errFasta := os.Open(pathFasta)
+	if errFasta != nil {
+		logrus.Fatal("Error reading fasta file", errFasta)
+	}
+
+	outputFile, errOutput := os.Create(pathOutput)
+	if errOutput != nil {
+		logrus.Fatal("Error creating output file (.fai)", errOutput)
+	}
+
+	repeatmaskFile, errRm := os.Open(pathRepeatmasker)
+	if errRm != nil {
+		logrus.Fatal("Error reading repeatmasker file", errRm)
+	}
+
+	fastaEntries, err := dataloader.ReadFasta(fastaFile)
+
+	if err != nil {
+		logrus.Fatal("Error extracting sequence from fasta file", err)
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"NumSequences": len(fastaEntries),
+	}).Info("Read sequence(s) from fasta")
+
+	for i, entry := range fastaEntries {
+		logrus.WithFields(logrus.Fields{
+			"Header": entry.Header,
+			"Length": len(entry.Sequence),
+		}).Info("Added sequence #" + strconv.Itoa(i+1))
+	}
+
+	genomeIndex := index.BuildGenomeIndex(fastaEntries, repeatmaskFile)
+	// genomeIndex := index.BuildGenomeIndex(fastaEntries, nil)
+
+	// if 1 == 2 {
+	// 	fmt.Println(repeatmaskFile)
+	// }
+
+	index.WriteGenomeIndex(genomeIndex, outputFile)
+}
+
+func mapTas2R4All(genomeIndexPath string, outputPath string) {
+
+	// genomeIndexPath := "/home/sam/Data/gtamap/tas2/tas2r4/index/ENSG00000127364.gtai"
+
+	readsFwPath := "/home/sam/Data/genomes/NG-25876_HGT1_TAS2R4ko_lib434869_7080_3/NG-25876_HGT1_TAS2R4ko_lib434869_7080_3_1.fastq.gz"
+	readsRvPath := "/home/sam/Data/genomes/NG-25876_HGT1_TAS2R4ko_lib434869_7080_3/NG-25876_HGT1_TAS2R4ko_lib434869_7080_3_2.fastq.gz"
+
+	// outputPath := "/home/sam/Data/gtamap/tas2/tas2r4/aligned.sam"
+
+	genomeIndex := index.ReadGenomeIndexByPath(genomeIndexPath)
+	reader, _ := fastq.InitFromPaths(&readsFwPath, &readsRvPath)
+	writer := datawriter.InitFromPath(outputPath)
+
+	numThreads := 1
+
+	mapper.MapAll(genomeIndex, reader, writer, &numThreads)
+}
+
+func multiBuildSingle() {
+	pathRepeatmasker := "/home/sam/Data/repeatmasker/hg38.fa.out"
+	pathFastaMulti := "/home/sam/Data/gtamap/multi-index/ENSG00000075624.fa"
+	pathOutputMulti := "/home/sam/Data/gtamap/multi-index/ENSG00000075624.gtai"
+
+	buildMultiFastaIndex(pathFastaMulti, pathOutputMulti, pathRepeatmasker)
+}
+
+func multiMapSingle() {
+	pathOutputMulti := "/home/sam/Data/gtamap/multi-index/ENSG00000075624.gtai"
+	pathOutputMapping := "/home/sam/Data/gtamap/multi-index/ENSG00000075624.sam"
+	mapTas2R4All(pathOutputMulti, pathOutputMapping)
+}
+
+func multiBuildAll() {
+	pathRepeatmasker := "/home/sam/Data/repeatmasker/hg38.fa.out"
+	pathFastaMulti := "/home/sam/Data/gtamap/multi-index/genes.fa"
+	pathOutputMulti := "/home/sam/Data/gtamap/multi-index/genes.gtai"
+	buildMultiFastaIndex(pathFastaMulti, pathOutputMulti, pathRepeatmasker)
+}
+
+func multiMapAll() {
+	pathOutputMulti := "/home/sam/Data/gtamap/multi-index/genes.gtai"
+	pathOutputMapping := "/home/sam/Data/gtamap/multi-index/genes.sam"
+	mapTas2R4All(pathOutputMulti, pathOutputMapping)
+}
+
+func main() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	logrus.SetLevel(logrus.InfoLevel)
+
+	// multiBuildSingle()
+	multiMapSingle()
+}
