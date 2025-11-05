8f17748d7e6ff29931644727932237a4b2ab13c6
Samuel Klein <it@samuel-klein.net>
2025-11-05 11:24:29 +0100

    testrunner update

--- src/testrunner.go
+++ src/testrunner.go
@@ -1,444 +1,210 @@
 package main
 
 import (
-	"fmt"
-	"github.com/KleinSamuel/gtamap/src/config"
-	"github.com/KleinSamuel/gtamap/src/core/index"
-	"github.com/KleinSamuel/gtamap/src/core/mapping"
-	"github.com/KleinSamuel/gtamap/src/core/timer"
-	"github.com/KleinSamuel/gtamap/src/dataloader"
-	"github.com/KleinSamuel/gtamap/src/datawriter"
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/KleinSamuel/gtamap/src/core/datastructure/regionvector"
 	"github.com/KleinSamuel/gtamap/src/formats/fastq"
-	"github.com/KleinSamuel/gtamap/src/formats/gtf"
 	"github.com/KleinSamuel/gtamap/src/formats/sam"
 	"github.com/KleinSamuel/gtamap/src/utils"
 	"github.com/sirupsen/logrus"
-	"os"
-	"runtime"
-	"strconv"
-	"sync"
-	"time"
 )
 
-var KmerLength int = 8
-
-func GetKmerLength() int {
-	return KmerLength
-}
-
-func inspectAnnotation() {
-	pathGtfZeroed := "../resources/ENSG00000173585.zeroed.gtf"
-	pathFastaZeroed := "../resources/ENSG00000173585.fasta"
-	pathFastaIndex := "../resources/ENSG00000173585.fasta.fai"
-
-	var annotation *gtf.Annotation = dataloader.GenerateInputForIndexFromFile(pathGtfZeroed, pathFastaZeroed, pathFastaIndex)
-
-	for _, trans := range annotation.Genes[0].Transcripts {
-		fmt.Println(trans.SequenceDna)
-	}
-}
-
-func buildAndSerializeIndex() {
-
-	pathGtfZeroed := "./resources/ENSG00000173585.zeroed.gtf"
-	pathFastaZeroed := "./resources/ENSG00000173585.zeroed.fasta"
-	pathOutput := "./resources/ENSG00000173585.dev.gtai"
-
-	gtfFile, errGtf := os.Open(pathGtfZeroed)
-	if errGtf != nil {
-		logrus.Fatal("Error reading gtf file", errGtf)
-	}
-
-	fastaFile, errFasta := os.Open(pathFastaZeroed)
-	if errFasta != nil {
-		logrus.Fatal("Error reading fasta file", errFasta)
-	}
-
-	outputFile, errIndex := os.Create(pathOutput)
-	if errIndex != nil {
-		logrus.Fatal("Error reading output file (.fai)", errIndex)
-	}
-
-	index.BuildAndSerializeIndex(gtfFile, fastaFile, outputFile)
-}
-
-func deserializeIndex() *index.GtaIndex {
-	return index.DezerializeFromPath("./resources/ENSG00000173585.dev.gtai")
-}
-
-//func testArgparse() {
-//
-//	parser := argparse.NewParser("gtamap", "Gene-based Trancript-Aware readMAPping")
-//
-//	var cmdIndex *argparse.Command = parser.NewCommand("index", "Build the GTAMap index (.gtai).")
-//
-//	var gtfFile *os.File = cmdIndex.File("", "gtf", os.O_RDONLY, 0600, &argparse.Options{
-//		Required: true,
-//		Help:     "Gene transfer format (GTF) file.",
-//	})
-//	var fastaFile *os.File = cmdIndex.File("", "fasta", os.O_RDONLY, 0600, &argparse.Options{
-//		Required: true,
-//		Help:     "Nucleotide sequences (FASTA) file. The corresponding index (.fai) file must be present.",
-//	})
-//	var outputFile *os.File = cmdIndex.File("o", "output", os.O_WRONLY|os.O_CREATE, 0600, &argparse.Options{
-//		Required: true,
-//		Help:     "Output file (.gtai).",
-//	})
-//	var logLevelIndex *string = cmdIndex.Selector("", "loglevel", []string{"ERROR", "INFO", "DEBUG"}, &argparse.Options{
-//		Required: false,
-//		Help:     "Log output level.",
-//		Default:  "ERROR",
-//	})
-//
-//	var cmdMap *argparse.Command = parser.NewCommand("map", "Map reads to the GTAMap index.")
-//	var indexFile *os.File = cmdMap.File("", "index", os.O_RDONLY, 0600, &argparse.Options{
-//		Required: true,
-//		Help:     "GTAMap index (.gtai) file",
-//	})
-//	var fastqFwFile *os.File = cmdMap.File("", "r1", os.O_RDONLY, 0600, &argparse.Options{
-//		Required: true,
-//		Help:     "FASTQ file containing the forward reads.",
-//	})
-//	var fastqRwFile *os.File = cmdMap.File("", "r2", os.O_RDONLY, 0600, &argparse.Options{
-//		Required: false,
-//		Help:     "FASTQ file containing the reverse reads.",
-//	})
-//	var logLevelMap *string = cmdMap.Selector("", "loglevel", []string{"ERROR", "INFO", "DEBUG"}, &argparse.Options{
-//		Required: false,
-//		Help:     "log output level",
-//		Default:  "ERROR",
-//	})
-//
-//	err := parser.Parse(os.Args)
-//	if err != nil {
-//		fmt.Print(parser.Usage(err))
-//		return
-//	}
-//
-//	if cmdIndex.Happened() {
-//
-//		fmt.Println("Indexing..")
-//
-//		fmt.Println("GTF file: ", *gtfFile)
-//		fmt.Println("FASTA file: ", *fastaFile)
-//		fmt.Println("Output file: ", *outputFile)
-//		fmt.Println("loglevel: ", *logLevelIndex)
-//
-//	} else if cmdMap.Happened() {
-//
-//		fmt.Println("Mapping..")
-//
-//		fmt.Println("loglevel: ", *logLevelMap)
-//		fmt.Println("index: ", *indexFile)
-//		fmt.Println("fastq fw: ", *fastqFwFile)
-//		fmt.Println("fastq rw: ", *fastqRwFile)
-//
-//	} else {
-//		fmt.Println(parser.Usage("no valid command supplied"))
-//		return
-//	}
-//}
-
-func testFastqReader() {
-
-	pathReadsFw := "../resources/reads_first_1.1.fq"
-	//pathReadsRv := "../resources/reads_first_1.2.fq"
-
-	reader, _ := fastq.InitFromPaths(&pathReadsFw, nil)
-
-	read, _ := reader.NextRead()
-
-	fmt.Println(read.ReadR1)
-	fmt.Println(read.ReadR2)
-}
-
-func testMapping() {
-
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	timerStartTotal := time.Now()
-
-	gtaIndex := deserializeIndex()
-
-	samHeader := sam.Header{
-		Version:                 sam.Version,
-		ReferenceSequenceName:   gtaIndex.Gene.Chromosome,
-		ReferenceSequenceLength: int(gtaIndex.Gene.EndGenomic - gtaIndex.Gene.StartGenomic + 1),
-		GenomeAnnotationVersion: "",
-		GenomeAssemblyVersion:   "",
-		OrganismTaxId:           "",
-		ToolVersion:             config.ToolVersion(),
-		Transcripts:             make([]*sam.TranscriptInfo, len(gtaIndex.Transcripts)),
-	}
-
-	// add the transcripts to the SAM header
-	for i, transcript := range gtaIndex.Transcripts {
-		samHeader.Transcripts[i] = &sam.TranscriptInfo{
-			Id:                  "T" + strconv.Itoa(i),
-			TranscriptEnsemblId: transcript.TranscriptIdEnsembl,
-			TranscriptLength:    transcript.SequenceLength,
-		}
-	}
-
-	//pathReadsR1 := "./resources/reads/manual/reads_ccr9.1.fq"
-	//pathReadsR2 := "./resources/reads/manual/reads_ccr9.2.fq"
-
-	pathReadsR1 := "/home/sam/Data/genome-sam/1m.fw.fq"
-	pathReadsR2 := "/home/sam/Data/genome-sam/1m.rw.fq"
-
-	//pathReadsR1 := "/home/sam/Data/sra/SRR29933931.fastq"
-	//var pathReadsR2 *string = nil
-
-	reader, _ := fastq.InitFromPaths(&pathReadsR1, &pathReadsR2)
-
-	writer := datawriter.InitFromPath("./out/reads_ccr9.sam")
-	writer.Write(samHeader.String())
-
-	numWorkers := 1
-
-	taskQueueMapping := make(chan ReadPairMappingTask)
-	taskQueueWriter := make(chan string)
-	timerChannel := make(chan *timer.Timer)
-
-	// wait group that keeps track of the mapping goroutines that are still running
-	var waitgroupMapping sync.WaitGroup
-
-	// start the mapping worker goroutine pool
-	for i := 0; i < numWorkers; i++ {
-		waitgroupMapping.Add(1)
-		go mapReadPairWorker(i, taskQueueMapping, taskQueueWriter, &waitgroupMapping, gtaIndex, timerChannel)
-	}
-
-	// wait group that keeps track of the writer goroutine that is still running
-	var waitgroupWriter sync.WaitGroup
-	// start the writer goroutine
-	waitgroupWriter.Add(1)
-	go writeOutputWorker(taskQueueWriter, &waitgroupWriter, writer)
-
-	var waitGroupTimer sync.WaitGroup
-	waitGroupTimer.Add(1)
-	go timerAccumulator(timerChannel, &waitGroupTimer)
-
-	// the number of read pairs that have been processed
-	taskCounter := 0
-
-	// add each read pair as a mapping task to the task queue
-	for readPair, _ := reader.NextRead(); readPair != nil; readPair, _ = reader.NextRead() {
-
-		// TODO: remove after testing (only process specific read pair)
-		//name := strings.Split(readPair.ReadR1.Header, " ")[0]
-		//if name != "@3-0002/1" {
-		//	continue
-		//}
-
-		mappingTask := ReadPairMappingTask{
-			ID:       taskCounter,
-			ReadPair: readPair,
-		}
-		taskQueueMapping <- mappingTask
-
-		taskCounter++
-
-		// TODO: remove after testing
-		//if taskCounter == 2 {
-		//	break
-		//}
-	}
-
-	close(taskQueueMapping)
-
-	waitgroupMapping.Wait()
-
-	close(timerChannel)
-	waitGroupTimer.Wait()
-
-	close(taskQueueWriter)
-
-	waitgroupWriter.Wait()
-	writer.Close()
-
-	fmt.Println("mapping finished")
-	fmt.Println("num tasks: ", taskCounter)
-
-	//fmt.Println("writer finished")
-
-	totalDuration := time.Since(timerStartTotal)
-
-	logrus.WithFields(logrus.Fields{
-		"duration": utils.FormatDuration(totalDuration),
-		"io":       utils.FormatDuration(*reader.Duration),
-	}).Info("Finished mapping")
-}
-
-type ReadPairMappingTask struct {
-	ID       int
-	ReadPair *fastq.ReadPair
-}
-
-func timerAccumulator(timerChannel <-chan *timer.Timer, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	totalMapR1 := time.Duration(0)
-	totalMapR2 := time.Duration(0)
-	totalDetermineReadLocation := time.Duration(0)
-	totalExactMatch := time.Duration(0)
-
-	for t := range timerChannel {
-		totalMapR1 += t.MapR1
-		totalMapR2 += t.MapR2
-		totalDetermineReadLocation += t.DetermineReadLocation
-		totalExactMatch += t.ExactMatch
-	}
-
-	logrus.Info("Total mapping time: ", totalMapR1+totalMapR2)
-	logrus.Info("Total mapping time R1: ", totalMapR1)
-	logrus.Info("Total mapping time R2: ", totalMapR2)
-	logrus.Info("Total determine read location time: ", totalDetermineReadLocation)
-	logrus.Info("Total exact match time: ", totalExactMatch)
-}
-
-func writeOutputWorker(taskQueue <-chan string, wg *sync.WaitGroup, writer *datawriter.Writer) {
-
-	logrus.Debug("Started writeOutputWorker")
-
-	for task := range taskQueue {
-		writer.Write(task)
-	}
-
-	wg.Done()
-
-	logrus.Debug("Finished writeOutputWorker")
-}
-
-func mapReadPairWorker(workerId int, taskQueue <-chan ReadPairMappingTask, taskQueueWriter chan<- string,
-	wg *sync.WaitGroup, gtaIndex *index.GtaIndex, timerChannel chan<- *timer.Timer) {
-
-	logrus.WithFields(logrus.Fields{
-		"workerId": workerId,
-	}).Debug("Started mapReadPairWorker")
-
-	for task := range taskQueue {
-
-		logrus.WithFields(logrus.Fields{
-			"workerId": workerId,
-			"task":     task.ID,
-		}).Debug("Processing task")
-
-		result := mapping.MapReadPairDev(task.ReadPair, gtaIndex, timerChannel)
-
-		taskQueueWriter <- result
-	}
-
-	// signal that the worker has finished
-	wg.Done()
-
-	logrus.WithFields(logrus.Fields{
-		"workerId": workerId,
-	}).Debug("Finished mapReadPairWorker")
-}
-
 func main() {
-
 	logrus.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
 	})
 	logrus.SetLevel(logrus.InfoLevel)
 
-	buildAndSerializeIndexGenome()
+	mappingInfoPath := "/mnt/raidproj/proj/projekte/mapping/gtamap/mapper-diff/dmd-err0/read.mappinginfo"
+	fastqR1Path := "/mnt/raidproj/proj/projekte/mapping/gtamap/mapper-diff/dmd-err0/fw.fastq"
+	fastqR2Path := "/mnt/raidproj/proj/projekte/mapping/gtamap/mapper-diff/dmd-err0/rw.fastq"
 
-	//buildAndSerializeIndex()
-	//index := deserializeIndex()
-	//testMapping()
+	samPath := "/mnt/raidproj/proj/projekte/mapping/gtamap/mapper-diff/dmd-err0/read.sam"
 
-	//seq := "ATGTGGATAG"
-	//
-	//tree := keywordtree.NewKeywordTree()
-	//tree.AddKeyword(seq, 0, 0)
-	//
-	////tree.PrintEdgeList()
-	//
-	//res := tree.Search("ATGTGGATAG")
-	//fmt.Println(res)
+	fastqReader, err := fastq.InitFromPaths(&fastqR1Path, &fastqR2Path)
+	if err != nil {
+		logrus.Fatalf("Error initializing FASTQ reader: %v", err)
+	}
 
-	//buildAndSerializeIndex()
-	////testMapping()
-	//
-	//gtaIndex := deserializeIndex()
-	//
-	//leafs := gtaIndex.SuffixTree.FindLeafNodesRecursive(gtaIndex.SuffixTree.Root)
-	//
-	//numErrors := 0
-	//
-	//for _, leaf := range leafs {
-	//
-	//	//fmt.Println("leaf: ", leaf)
-	//
-	//	for sequenceIndex, locations := range leaf.Locations {
-	//		lenTranscript := len(gtaIndex.GetTranscriptSequenceDna(sequenceIndex))
-	//
-	//		for _, location := range locations {
-	//			if location > lenTranscript {
-	//				fmt.Println("location: ", location)
-	//				fmt.Println("lenTranscript: ", lenTranscript)
-	//				numErrors++
-	//				//panic("location > lenTranscript")
-	//				fmt.Println(leaf)
-	//			}
-	//		}
-	//	}
-	//}
-	//
-	//gtaIndex.SuffixTree.ToEdgeList()
-	//
-	//fmt.Println("numErrors: ", numErrors)
+	// Create SAM output file
+	samFile, err := os.Create(samPath)
+	if err != nil {
+		logrus.Fatalf("Error creating SAM file: %v", err)
+	}
+	defer samFile.Close()
 
-	//allSequences := make([]string, 2)
-	//
-	//allSequences[0] = "abcdefabxybcdmnabcdex1"
-	//allSequences[1] = "abcdefgh2"
-	//
-	//tree := datastructure.CreateNewTree()
-	//
-	//for i, sequence := range allSequences {
-	//	tree.AddSequence(sequence, i)
-	//}
+	// Write SAM header
+	_, err = samFile.WriteString("@HD\tVN:1.6\n")
+	if err != nil {
+		logrus.Fatalf("Error writing SAM header: %v", err)
+	}
+	_, err = samFile.WriteString("@SQ\tSN:X\tLN:33339609\n")
+	if err != nil {
+		logrus.Fatalf("Error writing SAM header: %v", err)
+	}
 
-	//logrus.Debug()
-	//logrus.Debug("final tree:")
-	//tree.ToEdgeList()
+	file, err := os.Open(mappingInfoPath)
+	if err != nil {
+		logrus.Fatalf("Error opening mappinginfo file: %v", err)
+	}
+	defer file.Close()
 
-	//pattern := "e"
-	//result := tree.FindPatternExact(&pattern)
-	//fmt.Println(result)
+	scanner := bufio.NewScanner(file)
+	lineNumber := 0
 
-	//datastructure.BuildSuffixTree(allSequences)
+	// Skip first line of mappinginfo
+	if scanner.Scan() {
+		lineNumber++
+	}
 
-	//var suffixTree *datastructure.SuffixTree = datastructure.BuildSuffixTree(allSequences)
+	// Read FASTQ entries and mappinginfo simultaneously
+	for {
+		// Read FASTQ entry (4 lines: header, sequence, +, quality)
+		readPair, _ := fastqReader.NextRead()
+		if readPair == nil {
+			break
+		}
 
-	//leafs := suffixTree.FindLeafNodesRecursive(suffixTree.Root)
+		// Read mappinginfo entry (1 line)
+		if !scanner.Scan() {
+			break
+		}
+		lineNumber++
 
-	//for _, leaf := range leafs {
-	//
-	//	fmt.Println("leaf: ", leaf)
-	//
-	//	for sequenceIndex, locations := range leaf.Locations {
-	//		lenTranscript := len(allSequences[sequenceIndex])
-	//
-	//		for _, location := range locations {
-	//			if location > lenTranscript {
-	//				fmt.Println("location: ", location)
-	//				fmt.Println("lenTranscript: ", lenTranscript)
-	//				panic("location > lenTranscript")
-	//			}
-	//		}
-	//	}
-	//}
+		line := scanner.Text()
+		fields := strings.Split(line, "\t")
 
-	//pattern := "CA"
-	//
-	//result := suffixTree.FindPatternExact(&pattern)
-	//
-	//suffixTree.ToEdgeList()
-	//
-	//fmt.Println(result)
+		if len(fields) < 4 {
+			logrus.Warnf("Incomplete mappinginfo line %d: %s", lineNumber, line)
+			continue
+		}
+
+		qname := fields[0]
+		contig := fields[1]
+		fw_range := fields[4]
+		rv_range := fields[5]
+
+		fw_rv := rawRangeToRegionVector(fw_range)
+		rv_rv := rawRangeToRegionVector(rv_range)
+
+		samR1, samR2 := toSamRecordStrings(qname, contig, fw_rv, rv_rv, readPair.ReadR1, readPair.ReadR2)
+
+		// Write SAM records to file
+		_, err = samFile.WriteString(samR1 + "\n")
+		if err != nil {
+			logrus.Fatalf("Error writing SAM R1 record: %v", err)
+		}
+		_, err = samFile.WriteString(samR2 + "\n")
+		if err != nil {
+			logrus.Fatalf("Error writing SAM R2 record: %v", err)
+		}
+
+		// fmt.Println(samR1)
+		// fmt.Println(samR2)
+
+		// break
+	}
+
+	if err := scanner.Err(); err != nil {
+		logrus.Fatalf("Error reading mappinginfo file: %v", err)
+	}
+}
+
+func toSamRecordStrings(
+	qname string,
+	contig string,
+	fwRv *regionvector.RegionVector,
+	rvRv *regionvector.RegionVector,
+	readR1 *fastq.Read,
+	readR2 *fastq.Read,
+) (string, string) {
+	sbR1 := strings.Builder{}
+	sbR2 := strings.Builder{}
+
+	flagR1 := sam.Flag{}
+	flagR1.SetPaired()
+	flagR1.SetProperlyPaired()
+	flagR1.SetFirstInPair()
+	flagR1.SetMateReverseStrand()
+
+	fwStart := strconv.Itoa(fwRv.Regions[0].Start)
+
+	flagR2 := sam.Flag{}
+	flagR2.SetPaired()
+	flagR2.SetProperlyPaired()
+	flagR2.SetSecondInPair()
+	flagR2.SetReverseStrand()
+
+	rvStart := strconv.Itoa(rvRv.Regions[0].Start)
+
+	sbR1.WriteString(qname)
+	sbR1.WriteString("\t")
+	sbR1.WriteString(flagR1.String())
+	sbR1.WriteString("\t")
+	sbR1.WriteString(contig)
+	sbR1.WriteString("\t")
+	sbR1.WriteString(fwStart)
+	sbR1.WriteString("\t254\t")
+	sbR1.WriteString("100M")
+	sbR1.WriteString("\t")
+	sbR1.WriteString("*")
+	sbR1.WriteString("\t")
+	sbR1.WriteString(rvStart)
+	sbR1.WriteString("\t")
+	sbR1.WriteString("0")
+	sbR1.WriteString("\t")
+	sbR1.WriteString(string(*readR1.Sequence))
+	sbR1.WriteString("\t")
+	sbR1.WriteString(string(*readR1.Quality))
+
+	sbR2.WriteString(qname)
+	sbR2.WriteString("\t")
+	sbR2.WriteString(flagR2.String())
+	sbR2.WriteString("\t")
+	sbR2.WriteString(contig)
+	sbR2.WriteString("\t")
+	sbR2.WriteString(rvStart)
+	sbR2.WriteString("\t254\t")
+	sbR2.WriteString("100M")
+	sbR2.WriteString("\t")
+	sbR2.WriteString("*")
+	sbR2.WriteString("\t")
+	sbR2.WriteString(fwStart)
+	sbR2.WriteString("\t")
+	sbR2.WriteString("0")
+	sbR2.WriteString("\t")
+	sbR2.WriteString(utils.ReverseComplementDNA(string(*readR2.Sequence)))
+	sbR2.WriteString("\t")
+	sbR2.WriteString(utils.ReverseString(string(*readR2.Quality)))
+
+	return sbR1.String(), sbR2.String()
+}
+
+func rawRangeToRegionVector(rawRange string) *regionvector.RegionVector {
+	rv := regionvector.NewRegionVector()
+
+	fields := strings.Split(rawRange, "|")
+
+	for _, field := range fields {
+
+		startStop := strings.Split(field, "-")
+
+		start, errStart := strconv.Atoi(startStop[0])
+		if errStart != nil {
+			logrus.Fatalf("Error converting start to int: %v", errStart)
+		}
+
+		end, errEnd := strconv.Atoi(startStop[1])
+		if errEnd != nil {
+			logrus.Fatalf("Error converting end to int: %v", errEnd)
+		}
+
+		rv.AddRegion(start, end)
+	}
+
+	return rv
 }
