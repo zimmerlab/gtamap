0f713b52c834b5413eb2dfab35ff0956db828187
Samuel Klein <it@samuel-klein.net>
2025-12-07 20:51:12 +0100

    working on nvim dap, stdout still buggy

--- .nvim/dap.lua
+++ .nvim/dap.lua
@@ -1,65 +1,58 @@
 local dap = require("dap")
-
--- Configure the Go adapter (delve) - THIS IS REQUIRED
-dap.adapters.go = {
-	type = "server",
-	port = "${port}",
-	executable = {
-		command = vim.fn.stdpath("data") .. "/mason/bin/dlv",
-		args = { "dap", "-l", "127.0.0.1:${port}" },
-	},
-}
-
 -- Go configuration
--- dap.configurations.go = dap.configurations.go or {}
+dap.configurations.go = dap.configurations.go or {}
 
--- table.insert(dap.configurations.go, {
--- 	type = "go",
--- 	name = "testrunner genome",
--- 	request = "launch",
--- 	target = "debug",
--- 	-- program = "${workspaceFolder}/src/testrunnergenome.go",
--- 	program = "${file}",
--- })
+-- Debug current file
+table.insert(dap.configurations.go, {
+	type = "go",
+	name = "Debug Current File",
+	request = "launch",
+	mode = "debug",
+	program = "${file}",
+})
 
--- table.insert(dap.configurations.go, {
--- 	type = "go",
--- 	name = "test single read",
--- 	request = "launch",
--- 	target = "debug",
--- 	console = "integratedTerminal",
--- 	program = "${workspaceFolder}/src/main.go",
--- 	args = {
--- 		"map",
--- 		"--index",
--- 		"~/Data/kartmann/gtamap/ENSSSCG00000015652.gtai",
--- 		"--output",
--- 		"~/Data/kartmann/gtamap/",
--- 		"--read-origin",
--- 		"dna",
--- 		"--reads-r1",
--- 		"~/Data/kartmann/read2_Vis_36-15.fastq.gz",
--- 	},
--- })
-
-dap.configurations.go = {
-	{
-		type = "go",
-		name = "test single read",
-		request = "launch",
-		-- target = "debug",
-		console = "integratedTerminal",
-		program = "${workspaceFolder}/src/main.go",
-		args = {
-			"map",
-			"--index",
-			"~/Data/kartmann/gtamap/ENSSSCG00000015652.gtai",
-			"--output",
-			"~/Data/kartmann/gtamap/",
-			"--read-origin",
-			"dna",
-			"--reads-r1",
-			"~/Data/kartmann/read2_Vis_36-15.fastq.gz",
-		},
+table.insert(dap.configurations.go, {
+	type = "go",
+	name = "Kartmann",
+	request = "launch",
+	mode = "debug",
+	program = "/home/sam/Projects/gtamap/src/main.go",
+	console = "integratedTerminal",
+	showLog = true,
+	args = {
+		"map",
+		"--index",
+		"/home/sam/Data/kartmann/gtamap/ENSSSCG00000015652.gtai",
+		"--output",
+		"/home/sam/Data/kartmann/gtamap/",
+		"--read-origin",
+		"dna",
+		"--reads-r1",
+		"/home/sam/Data/kartmann/read2_Vis_36-15.fastq.gz",
 	},
-}
+})
+
+table.insert(dap.configurations.go, {
+	type = "go",
+	name = "giab chr20",
+	request = "launch",
+	mode = "debug",
+	program = "/home/sam/Projects/gtamap/src/main.go",
+	console = "integratedTerminal",
+	showLog = true,
+	args = {
+		"map",
+		"--index",
+		"/home/sam/Data/gtamap/evaluation/index/20:39659472-39675318.gtai",
+		"--output",
+		"/home/sam/Data/gtamap/evaluation/output/",
+		"--read-origin",
+		"dna",
+		"--reads-r1",
+		"/home/sam/Data/gtamap/evaluation/fastq/test.r1.fastq",
+		-- "/home/sam/Data/gtamap/evaluation/fastq/2A2_TGACCA_L001_R1_001.fastq.gz",
+		"--reads-r2",
+		"/home/sam/Data/gtamap/evaluation/fastq/test.r2.fastq",
+		-- "/home/sam/Data/gtamap/evaluation/fastq/2A2_TGACCA_L001_R2_001.fastq.gz",
+	},
+})
